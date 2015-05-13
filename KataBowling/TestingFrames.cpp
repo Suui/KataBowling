a5ea47b3d801922b774a8155b222dbe36dac7731
Suui <suui.gd@gmail.com>
2015-05-13 20:06:15 +0200

    Tests are now in different files for a better organization

--- KataBowling/TestingFrames.cpp
+++ KataBowling/TestingFrames.cpp
@@ -2,8 +2,6 @@
 
 #include "catch.hpp"
 #include "Frame.h"
-#include "FrameInterpreter.h"
-#include "Line.h"
 
 
 TEST_CASE("Testing Frame Scores", "[score][frame]")
@@ -22,7 +20,7 @@ TEST_CASE("Testing Frame Scores", "[score][frame]")
 }
 
 
-TEST_CASE("Testing Strike, Spare and Last Frame", "[score][frame]")
+TEST_CASE("Testing whether frame is a Strike, Spare or a Last Frame", "[score][frame]")
 {
 	auto SpareFrame = new Frame(0, 10);
 	auto StrikeFrame = new Frame(10, 0);
@@ -58,70 +56,5 @@ SCENARIO("Testing score values for the Last Frame (plain sum)", "[score][frame]"
 		
 		REQUIRE(LastFrame->IsLastFrame());
 		CHECK(LastFrame->GetScore() == 30);
-	}
-}
-
-
-TEST_CASE("Score Interpreter interpretations", "[score][interpreter]")
-{
-	REQUIRE(FrameInterpreter::interpret('G', '-').GetScore() == 0);
-	REQUIRE(FrameInterpreter::interpret('G', '3').GetScore() == 3);
-	REQUIRE(FrameInterpreter::interpret('G', '/').GetScore() == 10);
-	
-	REQUIRE(FrameInterpreter::interpret('3', '-').GetScore() == 3);
-	REQUIRE(FrameInterpreter::interpret('3', '5').GetScore() == 8);
-	REQUIRE(FrameInterpreter::interpret('3', '/').GetScore() == 10);
-
-	REQUIRE(FrameInterpreter::interpret('X').GetScore() == 10);
-}
-
-
-TEST_CASE("Line score tests", "[score][line]")
-{
-	SECTION("All zeroes")
-	{
-		REQUIRE(Line("G-G-G-G-G-G-G-G-G-G-").GetScore() == 0);
-	}
-
-	SECTION("No Spares or Strikes")
-	{
-		REQUIRE(Line("G-36G4G-G-G-18G-9-9-").GetScore() == 40);
-	}
-
-	SECTION("A Spare")
-	{
-		REQUIRE(Line("G-36G4G/3-G-18G-9-9-").GetScore() == 56);
-	}
-
-	SECTION("A Strike")
-	{
-		REQUIRE(Line("G-36G4G-X3-18G-9-9-").GetScore() == 66);
-	}
-
-	SECTION("A Spare and a Strike")
-	{
-		REQUIRE(Line("5/5-G-G/X5/3-G-G-3-").GetScore() == 89); // 10+5=15 +5=20 +10+10=40  +20+10=70 +10+3=83 +3=86 +3=89  [89]
-	}
-
-	SECTION("Spare or Strike in the Last Frame")
-	{
-		REQUIRE(Line("G-G-G-G-G-G-G-G-G-G/G").GetScore() == 10);
-		REQUIRE(Line("G-G-G-G-G-G-G-G-G-G/X").GetScore() == 20);
-		REQUIRE(Line("G-G-G-G-G-G-G-G-G-XXX").GetScore() == 30);
-
-		REQUIRE(Line("G-G-G-G-G-G-G-G-G/XXX").GetScore() == 50);
-		REQUIRE(Line("G-G-G-G-G-G-G-G-XXXX").GetScore() == 60);
-	}
-
-	SECTION("All Spares")
-	{
-		REQUIRE(Line("G/G/G/G/G/G/G/G/G/G/G").GetScore() == 100);
-		REQUIRE(Line("3/3/3/3/3/3/3/3/3/3/3").GetScore() == 130);
-		REQUIRE(Line("5/5/5/5/5/5/5/5/5/5/5").GetScore() == 150);
-	}
-
-	SECTION("All Strikes")
-	{
-		REQUIRE(Line("XXXXXXXXXXXX").GetScore() == 300);
 	}
 }
