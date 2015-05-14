8df6617d033ab937b1b105b9a678cb78e9952c36
Suui <suui.gd@gmail.com>
2015-05-14 14:54:14 +0200

    just learned about dma and fixed leaks

--- KataBowling/TestingFrames.cpp
+++ KataBowling/TestingFrames.cpp
@@ -8,27 +8,27 @@ TEST_CASE("Testing Frame Scores", "[score][frame]")
 {
 	SECTION("Zero Frame Score")
 	{
-		auto TestFrame = new Frame(0, 0);
-		CHECK(TestFrame->GetScore() == 0);
+		auto TestFrame = Frame(0, 0);
+		CHECK(TestFrame.GetScore() == 0);
 	}
 
 	SECTION("5 Frame Score")
 	{
-		auto TestFrame = new Frame(2, 3);
-		CHECK(TestFrame->GetScore() == 5);
+		auto TestFrame = Frame(2, 3);
+		CHECK(TestFrame.GetScore() == 5);
 	}
 }
 
 
 TEST_CASE("Testing whether frame is a Strike, Spare or a Last Frame", "[score][frame]")
 {
-	auto SpareFrame = new Frame(0, 10);
-	auto StrikeFrame = new Frame(10, 0);
-	auto LastFrame = new Frame(2, 2, 2);
+	auto SpareFrame = Frame(0, 10);
+	auto StrikeFrame = Frame(10, 0);
+	auto LastFrame = Frame(2, 2, 2);
 
-	REQUIRE(SpareFrame->IsSpare());
-	REQUIRE(StrikeFrame->IsStrike());
-	REQUIRE(LastFrame->IsLastFrame());
+	REQUIRE(SpareFrame.IsSpare());
+	REQUIRE(StrikeFrame.IsStrike());
+	REQUIRE(LastFrame.IsLastFrame());
 }
 
 
@@ -36,25 +36,25 @@ SCENARIO("Testing score values for the Last Frame (plain sum)", "[score][frame]"
 {
 	GIVEN("A last frame score of 0, 0, 0")
 	{
-		auto LastFrame = new Frame(0, 0, 0);
+		auto LastFrame = Frame(0, 0, 0);
 		
-		REQUIRE(LastFrame->IsLastFrame());
-		CHECK(LastFrame->GetScore() == 0);
+		REQUIRE(LastFrame.IsLastFrame());
+		CHECK(LastFrame.GetScore() == 0);
 	}
 
 	GIVEN("A last frame score of 0, 10, 10")
 	{
-		auto LastFrame = new Frame(0, 10, 10);
+		auto LastFrame = Frame(0, 10, 10);
 		
-		REQUIRE(LastFrame->IsLastFrame());
-		CHECK(LastFrame->GetScore() == 20);
+		REQUIRE(LastFrame.IsLastFrame());
+		CHECK(LastFrame.GetScore() == 20);
 	}
 
 	GIVEN("A last frame score of 10, 10, 10")
 	{
-		auto LastFrame = new Frame(10, 10, 10);
+		auto LastFrame = Frame(10, 10, 10);
 		
-		REQUIRE(LastFrame->IsLastFrame());
-		CHECK(LastFrame->GetScore() == 30);
+		REQUIRE(LastFrame.IsLastFrame());
+		CHECK(LastFrame.GetScore() == 30);
 	}
 }
---
+++ KataBowling/TestingLine.cpp
@@ -0,0 +1,7 @@
+﻿#include "catch.hpp"
+
+
+TEST_CASE("Will catch execute this?")
+{
+	REQUIRE(0 == 1);
+}
