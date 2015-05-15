eef2466eb63a7dec85164ab2e7150cf663702a78
Suui <suui.gd@gmail.com>
2015-05-15 03:57:11 +0200

    Creates a Frame with a spare

--- KataBowling/Frame.cpp
+++ KataBowling/Frame.cpp
@@ -1,20 +1,25 @@
 ﻿#include "Frame.h"
 
 
-Frame::Frame(const std::vector<char>& rolls): rolls(rolls) {}
+Frame::Frame(const std::vector<char>& input)
+{
+	for (char c : input)
+		rolls.push_back(parseToInt(c));
+}
 
 
 int Frame::knocks()
 {
 	int sum = 0;
-	for (auto&& roll : rolls)
-		sum += parseToInt(roll);
+	for (int roll : rolls)
+		sum += roll;
 	return sum;
 }
 
 
 int Frame::parseToInt(char roll)
 {
+	if (roll == '/') return 10 - rolls[0];
 	if (roll == '-') return 0;
 	return roll - '0';
 }
--- KataBowling/TestingFrames.cpp
+++ KataBowling/TestingFrames.cpp
@@ -13,4 +13,11 @@ TEST_CASE("Building a Frame", "[frame]")
 		REQUIRE(Frame({ '-', '3' }).knocks() == 3);
 		REQUIRE(Frame({ '4', '4' }).knocks() == 8);
 	}
+
+	SECTION("Creates a Frame with a spare")
+	{
+		REQUIRE(Frame({ '-', '/' }).knocks() == 10);
+		REQUIRE(Frame({ '3', '/' }).knocks() == 10);
+	}
+
 }
