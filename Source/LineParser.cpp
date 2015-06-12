70ca98b38ee589052121fae48f66a0ebcea25c08
Suui <suui.gd@gmail.com>
2015-06-12 23:32:13 +0200

    Refactor: LineParser has passed frame parsing responsibilities to FrameParser class

--- Source/LineParser.cpp
+++ Source/LineParser.cpp
@@ -1,19 +1,18 @@
 ﻿#include "LineParser.h"
+#include "FrameParser.h"
 
 
 void LineParser::FillFrames(std::string input, Frame* frames)
 {
-	int j = 0;
-	for (unsigned int i = 0; i < input.length(); i += 2)
+	unsigned int i = 0, j = 0;
+
+	while (j < 9)
 	{
-		if (input.at(i) == 'X')
-		{
-			frames[j] = Frame({ 'X' });
-			j++;
-			--i;
-			continue;
-		}
-		frames[j] = Frame({ input.at(i), input.at(i + 1) });
+		frames[j] = FrameParser().Parse(input.substr(i, 3));
+		if (frames[j].IsStrike()) --i;
+		i += 2;
 		j++;
 	}
+
+	frames[j] = FrameParser().Parse(input.substr(i, input.length() - i));
 }
