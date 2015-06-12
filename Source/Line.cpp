eb8499be5380680ae92c42f222402d9d9d7a3f6f
Suui <suui.gd@gmail.com>
2015-06-12 23:31:03 +0200

    Refactor

--- Source/Line.cpp
+++ Source/Line.cpp
@@ -12,17 +12,7 @@ int Line::score()
 {
 	int sum = 0;
 	for (int i = 0; i < FRAMES_IN_LINE; ++i)
-	{
-		sum += frames[i].knocks();
-		if (frames[i].isSpare()) sum += frames[i + 1].firstRoll();
-		if (frames[i].isStrike())
-		{
-			if (frames[i+1].isStrike())
-				sum += 10 + frames[i + 2].firstRoll();
-			else
-				sum += frames[i + 1].firstRoll() + frames[i + 1].secondRoll();
-		}
-	}
+		sum += frames[i].Knocks() + frames[i].Bonus();
 
 	return sum;
 }
