84608809c58a2e48cc1aee6ab5a72da0e9f5a1a5
Suui <suui.gd@gmail.com>
2015-06-13 00:12:54 +0200

    Green: All tests.

---
+++ Source/FrameBuilder.cpp
@@ -0,0 +1,32 @@
+﻿#include "FrameBuilder.h"
+#include "Frame.h"
+
+
+Frame FrameBuilder::Build(std::vector<int> rolls)
+{
+	this->rolls = rolls;
+	if (IsStrike()) return Frame({ 10 }, CalculateBonus(2));
+	if (IsSpare()) return Frame({ rolls[0], rolls[1] }, CalculateBonus(1));
+	return Frame({ rolls[0], rolls[1] }, 0);
+}
+
+
+int FrameBuilder::CalculateBonus(int bonusRolls)
+{
+	int sum = 0;
+	for (unsigned int i = 3 - bonusRolls; i < rolls.size(); ++i)
+		sum += rolls[i];
+	return sum;
+}
+
+
+bool FrameBuilder::IsStrike()
+{
+	return rolls[0] == 10;
+}
+
+
+bool FrameBuilder::IsSpare()
+{
+	return rolls[0] + rolls[1] == 10;
+}
