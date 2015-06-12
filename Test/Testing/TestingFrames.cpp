0280ecbb90549c91704cc89e74817a764fe07ae3
Suui <suui.gd@gmail.com>
2015-06-12 23:35:10 +0200

    Refactor

--- Test/Testing/TestingFrames.cpp
+++ Test/Testing/TestingFrames.cpp
@@ -23,19 +23,19 @@ TEST_CASE("Building a Frame", "[frame]")
 	SECTION("Creates a Frame with a strike")
 		REQUIRE(FrameParser().Parse({ 'X' }).Knocks() == 10);
 
-	SECTION("Calculates knocks for a last Frame with a spare")
+	SECTION("Calculates knocks and bonus for a Frame with a spare")
 	{
 		REQUIRE(FrameParser().Parse({ '3', '/', 'X' }).Knocks() == 10);
 		REQUIRE(FrameParser().Parse({ '3', '/', 'X' }).Bonus() == 10);
 	}
 
-	SECTION("Calculates knocks for a last Frame with a strike")
+	SECTION("Calculates knocks and bonus for a Frame with a strike")
 	{
 		REQUIRE(FrameParser().Parse({ 'X', '3', '5' }).Knocks() == 10);
 		REQUIRE(FrameParser().Parse({ 'X', '3', '5' }).Bonus() == 8);
 	}
 
-	SECTION("Calculates knocks for a last Frame with and a spare")
+	SECTION("Calculates knocks and bonus for a Frame with and a spare")
 	{
 		REQUIRE(FrameParser().Parse({ 'X', '3', '/' }).Knocks() == 10);
 		REQUIRE(FrameParser().Parse({ 'X', '3', '/' }).Bonus() == 10);
